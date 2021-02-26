969772a3bb804ff9b269c0a73da765a4d5b777ee
raulynas <laurynas@inwoodengineering.co.uk>
2021-02-26 16:16:09 +0100

    Hobbies section #12

--- js/components/hobbies/hobbies.js
+++ js/components/hobbies/hobbies.js
@@ -1,20 +1,55 @@
 function hobbies(data) {
 	//validation
-	if (!data) {
-		return false;
+	if (data === undefined) {
+		return "Neduoti parametrai";
 	}
+	if (typeof data !== "object" || Array.isArray(data)) {
+		return "Duotas netinkamo tipo parametras";
+	}
+	const allowedKeys = ["selector", "list"];
+	const keys = Object.keys(data);
+	if (keys.length !== allowedKeys.length) {
+		return "Netinkamas raktazodziu kiekis";
+	}
+	for (const key of keys) {
+		if (!allowedKeys.includes(key)) {
+			return "Netinkama parametro struktura";
+		}
+	}
+	if (typeof data.selector !== "string" || data.selector === "") {
+		return "Selectorius netinkamo formato";
+	}
+	if (!Array.isArray(data.list) || data.list.length === 0) {
+		return "List netinkamo formato";
+	}
+
 	//logic
 	const { selector, list } = data;
 	const DOM = document.querySelector(selector);
 	let HTML = "";
 	for (const item of list) {
-		HTML += `<div class="col-12 col-md-4 col-lg-3 hobbie">
-        <div class="icon">${item.icon}</div>
-        <p class="text">${item.text}
-        </div>`;
+		if (
+			typeof item === "object" &&
+			!Array.isArray(item) &&
+			item.icon &&
+			typeof item.icon === "string" &&
+			item.icon !== "" &&
+			item.text &&
+			typeof item.text === "string" &&
+			item.text !== ""
+		) {
+			HTML += `<div class="col-12 col-md-4 col-lg-3 hobbie">
+            <div class="icon">${item.icon}</div>
+            <p class="text">${item.text}
+            </div>`;
+		}
 	}
 
 	//post logic validation
+
+	if (HTML === "") {
+		return "Parametruose nebuvo naudingos informacijos";
+	}
 
 	//result return
 	DOM.innerHTML = HTML;
